7438d1a5ab21df0cdfe5fa4eb7f81b3ff5de2d02
Ailson <ailsonsixseven@gmail.com>
2024-02-03 12:44:35 -0300

    feat(chartobinary): add input data

--- chartobinary.c
+++ chartobinary.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
+char binary[9];
+
 void charToBinary(char c) {
-    char binary[9]; // Array to store binary representation
+    //char binary[9]; // Array to store binary representation
     sprintf(binary, "%08b", c); // Convert character to binary
     printf("%s\n", binary); // Print binary representation
 }
 
 int main() {
-    charToBinary('B'); // Test function with character 'A'
+    char inputChar;
+    
+    printf("Digite o caracter desejado: ");
+    scanf("%c", &inputChar); //charToBinary('B'); // Test function with character 'A'
+    
+    charToBinary(inputChar);
+
     return 0;
 }
 
